7c02bc5ca14860af89173f126547b02fbd95fb5c
Tofik Gashaw <gashawtofik@gmail.com>
2022-06-01 23:38:49 +0300

    Update 10-check_cycle.c

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -3,7 +3,7 @@
 /**
  * check_cycle - check if a singly linked list has a cycle in it
  * @list: list to be checked
- * Return: 0 if no cycle, 1 if there is a cycle
+ * Return: zero if no cycle, 1 if there is a cycle
  */
 int check_cycle(listint_t *list)
 {
